3dbddcd50276e5bf2388149350ce4c15768f6287
Danil <danilhristolubov5@gmail.com>
2023-07-19 16:52:38 +0700

    дз 5-го занятия

--- WebAdv/app_advertisements/urls.py
+++ WebAdv/app_advertisements/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, top_sellers, post_advertisement, register
 
 urlpatterns = [
-    path('', index)
+    path('', index, name='main-page'),
+    path('top-sellers/', top_sellers, name='top-sellers'),
+    path('advertisement-post/', post_advertisement, name='advertisement-post'),
+    path('register/', register, name='register')
 ]
--- WebAdv/app_advertisements/views.py
+++ WebAdv/app_advertisements/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
+
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
+
+def post_advertisement(request):
+    return render(request, 'advertisement-post.html')
+
+def register(request):
+    return render(request, 'register.html')
